0163ca93ad2dc7a81060eef437741049fa6f5db0
Tarine <tarinemacedo@gmail.com>
2019-08-08 20:40:47 +0100

    pull

--- src/main/java/org/academiadecodigo/whiledlings/persistence/dao/jpa/JpaQuestionDao.java
+++ src/main/java/org/academiadecodigo/whiledlings/persistence/dao/jpa/JpaQuestionDao.java
@@ -1,7 +1,7 @@
 package org.academiadecodigo.whiledlings.persistence.dao.jpa;
 
 import org.academiadecodigo.whiledlings.persistence.dao.QuestionDao;
-import org.academiadecodigo.whiledlings.persistence.model.Question;
+import org.academiadecodigo.whiledlings.persistence.model.questions.Question;
 
 public class JpaQuestionDao extends GenericJpaDao<Question> implements QuestionDao {
 
