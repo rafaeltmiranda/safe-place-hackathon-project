7c13d461c4844eb64d7d360ec3cf4ea3b1d41325
Tarine <tarinemacedo@gmail.com>
2019-08-08 21:23:14 +0100

    anotation @Repository in DAOs added

--- src/main/java/org/academiadecodigo/whiledlings/persistence/dao/jpa/JpaAnswerDao.java
+++ src/main/java/org/academiadecodigo/whiledlings/persistence/dao/jpa/JpaAnswerDao.java
@@ -2,7 +2,9 @@ package org.academiadecodigo.whiledlings.persistence.dao.jpa;
 
 import org.academiadecodigo.whiledlings.persistence.dao.AnswerDao;
 import org.academiadecodigo.whiledlings.persistence.model.Answer;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class JpaAnswerDao extends GenericJpaDao<Answer> implements AnswerDao {
 
     public JpaAnswerDao() {
--- src/main/java/org/academiadecodigo/whiledlings/persistence/dao/jpa/JpaOptionDao.java
+++ src/main/java/org/academiadecodigo/whiledlings/persistence/dao/jpa/JpaOptionDao.java
@@ -2,7 +2,9 @@ package org.academiadecodigo.whiledlings.persistence.dao.jpa;
 
 import org.academiadecodigo.whiledlings.persistence.dao.OptionDao;
 import org.academiadecodigo.whiledlings.persistence.model.Option;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class JpaOptionDao extends GenericJpaDao<Option> implements OptionDao {
 
     public JpaOptionDao() {
