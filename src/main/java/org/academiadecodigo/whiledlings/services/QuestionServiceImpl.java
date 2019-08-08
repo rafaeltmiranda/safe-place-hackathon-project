99be2ecd33c7d6d4b6cdda6e1f8ddde7f9336693
Ricardo Magalhães <ricardo_energie@hotmail.com>
2019-08-08 21:11:04 +0100

    exceptions added

--- src/main/java/org/academiadecodigo/whiledlings/services/QuestionServiceImpl.java
+++ src/main/java/org/academiadecodigo/whiledlings/services/QuestionServiceImpl.java
@@ -1,9 +1,7 @@
 package org.academiadecodigo.whiledlings.services;
 
 import org.academiadecodigo.whiledlings.persistence.dao.jpa.JpaOptionDao;
-import org.academiadecodigo.whiledlings.persistence.dao.jpa.JpaQuestionDao;
 import org.academiadecodigo.whiledlings.persistence.model.Option;
-import org.academiadecodigo.whiledlings.persistence.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,29 +10,11 @@ import java.util.List;
 @Service
 public class QuestionServiceImpl implements QuestionService {
 
-    private JpaQuestionDao jpaQuestionDao;
     private JpaOptionDao jpaOptionDao;
-    private OptionService  optionService;
-
-    @Autowired
-    public void setJpaQuestionDao(JpaQuestionDao jpaQuestionDao) {
-        this.jpaQuestionDao = jpaQuestionDao;
-    }
 
     @Autowired
     public void setJpaOptionDao(JpaOptionDao jpaOptionDao) {
         this.jpaOptionDao = jpaOptionDao;
-    }
-
-    @Autowired
-    public void setOptionService(OptionService optionService) {
-        this.optionService = optionService;
-    }
-
-    @Override
-    public void addQuestion(Question question) {
-
-        jpaQuestionDao.saveOrUpdate(question);
     }
 
     @Override
