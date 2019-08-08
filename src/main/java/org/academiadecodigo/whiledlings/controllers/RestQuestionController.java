a38e224803ec709ecf842294a5f72d4114c7c647
fernando abreu <fernandoabreu92@gmail.com>
2019-08-08 21:56:14 +0100

    finish questioncontroller implementation

--- src/main/java/org/academiadecodigo/whiledlings/controllers/RestQuestionController.java
+++ src/main/java/org/academiadecodigo/whiledlings/controllers/RestQuestionController.java
@@ -2,6 +2,8 @@ package org.academiadecodigo.whiledlings.controllers;
 
 import org.academiadecodigo.whiledlings.persistence.model.Option;
 import org.academiadecodigo.whiledlings.persistence.model.questions.Question;
+import org.academiadecodigo.whiledlings.persistence.model.questions.QuestionType;
+import org.academiadecodigo.whiledlings.persistence.model.questions.QuestionTypeOptions;
 import org.academiadecodigo.whiledlings.services.QuestionService;
 import org.graalvm.compiler.nodes.calc.IntegerDivRemNode;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,16 +27,27 @@ public class RestQuestionController {
         this.questionService = questionService;
     }
 
-    // TODO: 08/08/2019 addQuestion method
-
-    @RequestMapping(method = RequestMethod.GET, path = "/{qid}")
-    public ResponseEntity<List<Option>> showOptions(@PathVariable Integer qid){
+    @RequestMapping(method = RequestMethod.GET, path = "/{qid}/options")
+    public ResponseEntity<?> showOptions(@PathVariable Integer qid){
 
         //List<Option> optionsList = questionService.getOptionsList();
 
         Question question = questionService.getQuestion(qid); // TODO: 08/08/2019 ricardo doing this
-        List<Option> optionsList = question.getOptions(); // TODO: 08/08/2019 hum
+
+        if (question.getQuestionType() == QuestionType.TEXT){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        QuestionTypeOptions questionTypeOptions = (QuestionTypeOptions) question;
+        List<Option> optionsList = questionTypeOptions.getOptions();
 
         return new ResponseEntity<>(optionsList, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/{qid}")
+    public ResponseEntity<Question> showQuestion(@PathVariable Integer qid){
+
+        Question question = questionService.getQuestion(qid);
+        return new ResponseEntity<>(question, HttpStatus.OK);
     }
 }
