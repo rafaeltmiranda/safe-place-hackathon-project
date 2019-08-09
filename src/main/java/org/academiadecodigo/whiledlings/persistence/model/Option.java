548759858891eefa952f55375c503e62f2614c50
Tarine <tarinemacedo@gmail.com>
2019-08-09 02:11:51 +0100

    database

--- src/main/java/org/academiadecodigo/whiledlings/persistence/model/Option.java
+++ src/main/java/org/academiadecodigo/whiledlings/persistence/model/Option.java
@@ -7,7 +7,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "option")
+@Table(name = "options")
 public class Option extends AbstractModel {
 
     @ManyToOne
